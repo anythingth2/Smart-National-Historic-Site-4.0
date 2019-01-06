0553ff8d71f7327abf77635b9c4f249aade3ff46
porlolicon <porlolicon@gmail.com>
2019-01-06 20:23:13 +0700

    add https support

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('babel-register');
 require("babel-polyfill");
 require('dotenv').config()
+if (process.argv.indexOf('--prod') !== -1) {
+  process.env.NODE_ENV = 'prod'
+}
 require('./src/app')
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import bodyParser from 'body-parser'
 import { middleware } from '@line/bot-sdk'
 import webhook from './controllers/webhook'
 import Api from './routes'
+import http from 'http'
+import https from 'https'
+import fs from 'fs'
 
 const { channelAccessToken, channelSecret } = process.env
 
@@ -23,6 +26,23 @@ app.get('/', (req, res) => {
   res.send('Hi')
 })
 
-app.listen(8080, () => {
-  console.log("Server started at port 8080")
-})
+
+if (process.env.NODE_ENV === 'prod') {
+  http.createServer((req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+  }).listen(80);
+  const { HTTPS_KEY, HTTPS_CERT, HTTPS_CA } = process.env
+  const options = {
+    key: fs.readFileSync(HTTPS_KEY),
+    cert: fs.readFileSync(HTTPS_CERT),
+    ca: fs.readFileSync(HTTPS_CA)
+  };
+  https.createServer(options, app).listen(443, () => {
+    console.log('Server listening on port ' + 443 + ' in prod mode');
+  })
+} else {
+  app.listen(8080, function () {
+    console.log('Server listening on port ' + 8080 + ' in dev mode');
+  })
+}
