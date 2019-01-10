65fa56461aebe8a38ac11ad6bfc90b19c132ab2a
porlolicon <porlolicon@gmail.com>
2019-01-10 15:00:21 +0700

    init beacon controller

--- src/controllers/beacon.js
+++ src/controllers/beacon.js
@@ -1,2 +1,15 @@
 import { Beacon } from '../models'
 
+const addEntry = (req, res) => {
+  const { datetime, status } = req.body.beacon
+  const currentHour = new Date().getHours()
+  Beacon.find({ createdAt: {} })
+  Sensor.create(sensorData, err => {
+    if (err) {
+      console.error(err)
+      res.status(500).end()
+    } else {
+      res.status(200).end()
+    }
+  })
+}
---
+++ src/routes/beacon.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import Beacon from '../controllers/beacon'
+
+const router = Router()
+
+// router.post('/webhook', Beacon.)
+
+export default router
