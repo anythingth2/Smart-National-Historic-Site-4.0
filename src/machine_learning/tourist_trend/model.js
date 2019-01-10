3fec5e0709f3dce68b2216219129fbc4e3aae73f
ChiChaChai <chatchaishaetan@gmail.com>
2019-01-10 16:05:09 +0700

    Add preprocess

--- src/machine_learning/tourist_trend/model.js
+++ src/machine_learning/tourist_trend/model.js
@@ -1,4 +1,9 @@
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
 const csvtojson = require('csvtojson');
+
+const SERIES_SIZE = 12;
+
 
 class Model {
 
@@ -19,7 +24,7 @@ class Model {
         };
         var data = (await csvtojson({
             delimiter: ';'
-        }).fromFile(this.CSV_PATH))
+        }).fromFile(this.CSV_PATH));
 
         return data.map(row => {
             let newRow = removeExtendedField(row);
@@ -27,12 +32,32 @@ class Model {
         });
     }
     async preprocess() {
-        console.log(await this.readCsv());
+        let data = await this.readCsv();
+        let freqPeoples = data.reduce((acc, current) => acc.concat(current.values), []);
+        var xs = [];
+        var ys = [];
+        for (var i = 0; i < freqPeoples.length - SERIES_SIZE; i++) {
+            xs.push(freqPeoples.slice(i, i + SERIES_SIZE));
+            ys.push(freqPeoples[i + SERIES_SIZE]);
+        }
+        return {
+            xs: tf.tensor2d(xs).reshape([-1, SERIES_SIZE, 1]),
+            ys: tf.tensor1d(ys)
+        };
+    }
+
+    compile() {
+        this.model.compile({
+            optimizer: tf.train.adam(LEARNING_RATE),
+            // optimizer: tf.train.rmsprop(0.01),
+            loss: 'meanSquaredError',
+            metrics: ['accuracy', 'mse']
+        });
     }
     constructor() {
         this.CSV_PATH = `${__dirname}/sanam.csv`;
         this.preprocess().then((data) => {
-            // console.log()
+
         });
     }
 }
