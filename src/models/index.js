94d031d2b939b79cf18edb2588053aca86cdd434
porlolicon <porlolicon@gmail.com>
2019-01-10 18:06:09 +0700

    add beacon log

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 import Sensor from './sensor'
 import User from './user'
 import Beacon from './beacon'
+import pCounter from './pCounter'
 
 export {
   Sensor,
   User,
-  Beacon
+  Beacon,
+  pCounter
 }
