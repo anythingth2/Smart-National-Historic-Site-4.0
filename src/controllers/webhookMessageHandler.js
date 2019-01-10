6ac6bed227bbd1447374b0a66ad328bd70588d6f
Supavitch Sangsuwan <field1146@gmail.com>
2019-01-10 09:59:50 +0700

    change flex.js -> lineMessage.js

--- src/controllers/webhookMessageHandler.js
+++ src/controllers/webhookMessageHandler.js
@@ -1,24 +1,19 @@
 import { replyTo, sendTo } from './webhook'
 import Sensor from './sensor'
+import lineMessage from '../lineMessage'
 
 const handleAskCustom = (queryResult, replyToken, callback) => {
   const sensor = queryResult.parameters.fields.sensor.stringValue
   //request data from DB
   Sensor._getEntry(sensor, 1, (err, doc) => {
     //reply
-    replyTo(replyToken, {
-      type: 'text',
-      text: '' + sensor + ' : ' + doc[0][sensor]
-    })
+    replyTo(replyToken,lineMessage.getText('' + sensor + ' : ' + doc[0][sensor]))
   })
 }
 
 const handleNormalIntent = (queryResult, replyToken, callback) => {
   console.log('reply : ', queryResult.fulfillmentText)
-  replyTo(replyToken, {
-    type: 'text',
-    text: queryResult.fulfillmentText
-  }).then(() => {
+  replyTo(replyToken,lineMessage.getText(queryResult.fulfillmentText)).then(() => {
     callback()
   })
 }
